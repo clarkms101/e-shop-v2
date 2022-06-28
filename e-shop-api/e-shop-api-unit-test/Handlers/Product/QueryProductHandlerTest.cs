5b925db63d25dae34b28a0beaf9c3bd691bb9b65
clarklin <clarkms101@gmail.com>
2022-06-29 07:55:09 +0800

    加入QueryProductHandlerTest

---
+++ e-shop-api/e-shop-api-unit-test/Handlers/Product/QueryProductHandlerTest.cs
@@ -0,0 +1,111 @@
+using System.Threading;
+using System.Threading.Tasks;
+using e_shop_api.Applications.Product.Query;
+using e_shop_api_unit_test.Utility;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Xunit;
+
+namespace e_shop_api_unit_test.Handlers.Product;
+
+public class QueryProductHandlerTest : TestBase
+{
+    private readonly QueryProductHandler _target;
+
+    /// <summary>
+    /// 測試的前置作業
+    /// </summary>
+    public QueryProductHandlerTest()
+    {
+        var fakeLog = Substitute.For<ILogger<QueryProductHandler>>();
+
+        FakeEShopDbContext.Products.Add(new e_shop_api.DataBase.Models.Product()
+        {
+            Id = 1,
+            Category = "金牌",
+            Content = "TestContent",
+            Description = "TestDescription",
+            ImageUrl = "TestUrl",
+            IsEnabled = true,
+            Num = 50,
+            OriginPrice = 990,
+            Price = 880,
+            Title = "TestProduct",
+            Unit = "個"
+        });
+        FakeEShopDbContext.Products.Add(new e_shop_api.DataBase.Models.Product()
+        {
+            Id = 2,
+            Category = "寵物用品",
+            Content = "TestContent",
+            Description = "TestDescription",
+            ImageUrl = "TestUrl",
+            IsEnabled = true,
+            Num = 70,
+            OriginPrice = 1990,
+            Price = 1880,
+            Title = "TestProduct2",
+            Unit = "個"
+        });
+        FakeEShopDbContext.SaveChanges();
+
+        _target = new QueryProductHandler(FakeEShopDbContext, fakeLog);
+    }
+
+    [Fact]
+    public async Task 查詢指定的產品編號_返回該產品的資訊()
+    {
+        // Arrange
+        var request = new QueryProductRequest()
+        {
+            ProductId = 1
+        };
+        var expected = new QueryProductResponse()
+        {
+            Success = true,
+            Message = "查詢成功!",
+            Product = new e_shop_api.Applications.Product.CommonDto.Product()
+            {
+                ProductId = 1,
+                Category = "金牌",
+                Content = "TestContent",
+                Description = "TestDescription",
+                ImageUrl = "TestUrl",
+                IsEnabled = true,
+                Num = 50,
+                OriginPrice = 990,
+                Price = 880,
+                Title = "TestProduct",
+                Unit = "個"
+            }
+        };
+
+        // Act
+        var actual = await _target.Handle(request, CancellationToken.None);
+
+        // Assert
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public async Task 查詢不存在的產品編號_返回錯誤訊息()
+    {
+        // Arrange
+        var request = new QueryProductRequest()
+        {
+            ProductId = 99
+        };
+        var expected = new QueryProductResponse()
+        {
+            Success = false,
+            Message = "查無該筆資料!"
+        };
+
+        // Act
+        var actual = await _target.Handle(request, CancellationToken.None);
+
+        // Assert
+        actual.Should().BeEquivalentTo(expected);
+    }
+}
