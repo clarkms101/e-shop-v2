035152b99a107031adc48aff32937dae47249993
clarklin <clarkms101@gmail.com>
2022-08-28 23:16:31 +0800

    調整分頁元件

--- e-shop/src/shared/components/pagination/pagination.component.ts
+++ e-shop/src/shared/components/pagination/pagination.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagination.component.css']
 })
 export class PaginationComponent implements OnInit {
-
+  page: number = 0;
+  hasPrePage: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getData(page: number): void {
+
+  }
 }
