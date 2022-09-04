79fd81910bf252a5db67059c94ad14558d5e2ea3
clarklin <clarkms101@gmail.com>
2022-09-04 11:11:45 +0800

    訂單頁面調整

--- e-shop/src/app/Admin/orders/orders.component.ts
+++ e-shop/src/app/Admin/orders/orders.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { appModuleAnimation } from 'src/shared/animations/routerTransition';
-import { Client, OrderInfo, Pagination, QueryOrdersRequest } from 'src/shared/api client/service-proxies';
+import { Client, OrderInfo, Pagination, QueryOrdersRequest, SelectionItem } from 'src/shared/api client/service-proxies';
 
 @Component({
   selector: 'app-orders',
@@ -11,6 +11,11 @@ import { Client, OrderInfo, Pagination, QueryOrdersRequest } from 'src/shared/ap
 export class OrdersComponent implements OnInit {
   // data
   orders: OrderInfo[] = [];
+  paymentMethodList: SelectionItem[] = [];
+  // query
+  query_startDate: string = '';
+  query_endDate: string = '';
+  query_paymentMethod: number | null | undefined = null;
   // page
   pagination: Pagination = new Pagination();
   totalPageArray: number[] = [];
@@ -23,6 +28,11 @@ export class OrdersComponent implements OnInit {
   ngOnInit(): void {
     let token = localStorage.getItem("adminJWT") as string;
     this._apiClient.setAuthToken(token);
+
+    this._apiClient.paymentMethod().subscribe((response) => {
+      this.paymentMethodList = response.items as SelectionItem[];
+    });
+
     this.getPageData(1);
   }
 
@@ -30,6 +40,15 @@ export class OrdersComponent implements OnInit {
     let request = new QueryOrdersRequest();
     request.page = page;
     request.pageSize = 10;
+    if (this.query_paymentMethod !== null && this.query_paymentMethod !== undefined) {
+      request.paymentMethod = this.query_paymentMethod;
+    }
+    if (this.query_startDate !== '') {
+      request.startDate = this.query_startDate;
+    }
+    if (this.query_endDate !== '') {
+      request.endDate = this.query_endDate;
+    }
 
     this._apiClient.orders(request).subscribe((response) => {
       if (response.orderInfos !== undefined && response.pagination !== undefined) {
