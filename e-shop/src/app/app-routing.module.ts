f2dd0d5f8409daf916224ed85fadf2783607a283
clarklin <clarkms101@gmail.com>
2022-09-23 18:12:56 +0800

    路由順序調整

--- e-shop/src/app/app-routing.module.ts
+++ e-shop/src/app/app-routing.module.ts
@@ -9,14 +9,14 @@ import { Page404Component } from './Common/page/page404/page404.component';
 import { IndexComponent } from './Portal/index/index.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'index', pathMatch: 'full' },
-  { path: '**', redirectTo: '404' },
-  { path: '404', component: Page404Component },
   { path: 'index', component: IndexComponent },
   { path: 'admin/login', component: LoginComponent },
   { path: 'admin/products', component: ProductsComponent, canActivate: [AuthGuardService] },
   { path: 'admin/orders', component: OrdersComponent, canActivate: [AuthGuardService] },
   { path: 'admin/coupons', component: CouponsComponent, canActivate: [AuthGuardService] },
+  { path: '404', component: Page404Component },
+  { path: '', redirectTo: 'index', pathMatch: 'full' },
+  { path: '**', redirectTo: '404' }
 ];
 
 @NgModule({
