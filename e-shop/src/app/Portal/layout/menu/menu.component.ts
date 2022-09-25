b07e2ca33accf0b8cd066e778147ba9cd4d4fb75
clarklin <clarkms101@gmail.com>
2022-09-25 16:22:39 +0800

    menu重構

--- e-shop/src/app/Portal/layout/menu/menu.component.ts
+++ e-shop/src/app/Portal/layout/menu/menu.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  menuItemArray: Array<menuItem> = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    let menuItem1 = new menuItem();
+    menuItem1.itemName = '金牌專賣店';
+    menuItem1.itemCategory = '金牌';
+    menuItem1.itemIsActive = true;
+    this.menuItemArray.push(menuItem1);
+
+    let menuItem2 = new menuItem();
+    menuItem2.itemName = '禮品區';
+    menuItem2.itemCategory = '禮品';
+    menuItem2.itemIsActive = false;
+    this.menuItemArray.push(menuItem2);
+
+    let menuItem3 = new menuItem();
+    menuItem3.itemName = '寵物用品';
+    menuItem3.itemCategory = '寵物用品';
+    menuItem3.itemIsActive = false;
+    this.menuItemArray.push(menuItem3);
   }
 
+  activeItem(itemIndex: number): void {
+    this.menuItemArray.forEach(item => {
+      item.itemIsActive = false;
+    });
+    this.menuItemArray[itemIndex].itemIsActive = true;
+  }
+}
+
+class menuItem {
+  itemName?: string;
+  itemCategory?: string;
+  itemIsActive?: boolean;
 }
